417d5013beb3e4da809b042e1a476ee2ee153fc3
okaryo <okaryotr@gmail.com>
2021-07-29 23:20:59 +0900

    [modify] main fun add scraping and parse process

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,32 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import model.Author
 import model.Book
+import model.Record
+import model.RecordList
 import org.jsoup.Jsoup
+import java.time.LocalDate
 
-fun main() {
+suspend fun main() {
     println("Hello World!")
-    // TODO: coroutine使う
     // TODO: スクレイピング中に表示するメッセージを考える
     // TODO: consoleからの使用を考えているので、外部からユーザーIDを受け付けるようにする
     // TODO: スクレイピング時のマナーをちゃんとする
-//    val document = Jsoup.connect("https://bookmeter.com/users/739784/books/read").get()
-//    val list = mutableListOf<Book>()
-//    document.select(".book__detail").map { element ->
-//        val date = element.select(".detail__date").first()!!.text()
-//        val titleElement = element.select(".detail__title").first()!!
-//        val title = titleElement.select("a").first()!!.text()
-//        val authorElement = element.select(".detail__authors").first()!!
-//        val author = authorElement.select("a").first()!!.text()
-//        val book = Book(title, Author(author))
-//        list.add(book)
-//    }
-//    println(list)
+    withContext(Dispatchers.IO) {
+        val document = Jsoup.connect("https://bookmeter.com/users/739784/books/read").get()
+        var totalPages = 0
+        val records = document.select(".group__book").map { element ->
+            val dateString = element.select(".detail__date").first()!!.text()
+            val titleElement = element.select(".detail__title").first()!!
+            val title = titleElement.select("a").first()!!.text()
+            val authorElement = element.select(".detail__authors").first()!!
+            val author = authorElement.select("a").first()!!.text()
+            val page = element.select(".detail__page").first()!!.text().toInt()
+            totalPages += page
+            val book = Book(title, Author(author), page)
+            val splitDate = dateString.split("/").map(String::toInt)
+            Record(book = book, thoughts = "", date = LocalDate.of(splitDate[0], splitDate[1], splitDate[2]))
+        }
+        println(RecordList(recordsCount = records.size, totalPages = totalPages, records = records))
+    }
 }
