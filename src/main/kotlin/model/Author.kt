c90243b639ca62797a4074b575c18bc2c4cce530
okaryo <okaryotr@gmail.com>
2021-07-31 22:04:39 +0900

    [modify] model class

--- src/main/kotlin/model/Author.kt
+++ src/main/kotlin/model/Author.kt
@@ -1,3 +1,6 @@
 package model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Author(val name: String)
--- src/main/kotlin/model/Book.kt
+++ src/main/kotlin/model/Book.kt
@@ -1,5 +1,8 @@
 package model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Book(
     val title: String,
     val author: Author,
